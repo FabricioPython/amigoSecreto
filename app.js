7516823a9c1c9831db82f13a0164938653f90dc8
FabricioPython <fabricio.mh@outlook.com>
2025-02-02 13:06:00 -0300

    implementação basica do core

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+// pega o resultado dos input
+function pegaResultado(tagInput) {
+  return document.querySelector(tagInput).value;
+}
+
+function sortearAmigo() {
+  resultado = parseInt(Math.random() * amigos.length);
+  return amigos[resultado];
+}
+
+function adicionarAmigo() {
+  // se  não for vazio adicione
+  if (document.querySelector("input").value !== "") {
+    amigos.push(pegaResultado("input"));
+    limparCampo();
+  }
+}
+
+function limparCampo() {
+  chute = document.querySelector("input");
+  chute.value = "";
+}
